8e49baf9681f33e1596edf81cc1a89db3c0afbe8
Toshiba <Toshiba@DESKTOP-S27CS35>
2021-04-12 01:32:16 +0300

    Add project files.

---
+++ Serializer/Gender1.cs
@@ -0,0 +1,7 @@
+﻿namespace Serializer
+    public enum Gender
+    {
+    Male,
+    Female
+}
+
---
+++ Serializer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Serializer
+    public class Student
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string MiddleName { get; set; }
+    public int Age { get; set; }
+    public Gender Gender { get; set; }
+}
+    public enum Gender 
+{ 
+    Male,
+    Female 
+}
+
+
+    class Program
+{
+    static void Main()
+    {
+        Student student = new Student
+        {
+            ID = 5,
+            Name = "Marina",
+            MiddleName = "Kovalchyk",
+            Age = 19,
+            Gender = Gender.Female
+        };
+        XmlSerializer serializer = new XmlSerializer(typeof(Student));
+        using (System.IO.FileStream fs = new FileStream("student.xml", FileMode.OpenOrCreate))
+        {
+            serializer.Serialize(fs, student);
+        }
+        using (FileStream fs = new FileStream("student.xml", FileMode.OpenOrCreate))
+        {
+            var studentDeserialized = (Student)serializer.Deserialize(fs);
+            Console.WriteLine(studentDeserialized.Gender);
+        }
+    }
+}
+
